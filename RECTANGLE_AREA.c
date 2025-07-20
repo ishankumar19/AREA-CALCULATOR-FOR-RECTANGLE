a5bdc379b1c39499b7940bc9045428f6eb95d3b9
ISHANT KUMAR <ishansingh196055@gmail.com>
2025-07-20 12:49:56 +0530

    Add files via upload

---
+++ RECTANGLE_AREA.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+    int length,breath;
+
+    
+    printf("enter the length of the rectangle\n");
+    scanf("%d",&length);
+
+
+     printf("enter the breath of the rectangle\n");
+    scanf("%d",&breath);
+
+    printf("the area of the given triangle will be %d",length*breath);
+     
+    return 0;
+}
